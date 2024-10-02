39b8212c0c34880af7501ffd0621b5cea5513d94
Alfie Sargent <Alfonse179@gmail.com>
2024-10-02 16:28:30 +0100

    Added in a model and fixed some index issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import { Suspense, useEffect } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls, Stage, CameraShake, useAnimations } from '@react-three/drei'
+import { Suspense, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF, OrbitControls, CameraShake, useAnimations } from '@react-three/drei';
+import * as THREE from 'three';
+import { GLTF } from 'three-stdlib';
+
+useGLTF.preload('/robot-draco.glb');
+
+function Model(props: JSX.IntrinsicElements['group']) {
+  const { scene, animations } = useGLTF('/robot-draco.glb') as GLTF & { animations: any };
+  const { actions } = useAnimations(animations, scene);
+
+  useEffect(() => {
+    if (actions?.Idle) {
+      actions.Idle.play();
+    }
+  }, [actions]);
+
+  return <primitive object={scene} {...props} />;
+}
 
 export default function Viewer() {
-    return (
-      <Canvas shadows camera={{ fov: 50 }}>
-        <Suspense fallback={null}>
-        </Suspense>
-        <OrbitControls makeDefault />
-        <CameraShake
-          maxYaw={0.1} // Max amount camera can yaw in either direction
-          maxPitch={0.1} // Max amount camera can pitch in either direction
-          maxRoll={0.1} // Max amount camera can roll in either direction
-          yawFrequency={0.1} // Frequency of the the yaw rotation
-          pitchFrequency={0.1} // Frequency of the pitch rotation
-          rollFrequency={0.1} // Frequency of the roll rotation
-          intensity={1} // initial intensity of the shake
-          decayRate={0.65} // if decay = true this is the rate at which intensity will reduce at />
-        />
-      </Canvas>
-    )
-  }
+  return (
+    <Canvas shadows camera={{ fov: 50 }}>
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+      <OrbitControls makeDefault />
+      <CameraShake
+        maxYaw={0.1}
+        maxPitch={0.1}
+        maxRoll={0.1}
+        yawFrequency={0.1}
+        pitchFrequency={0.1}
+        rollFrequency={0.1}
+        intensity={1}
+        decayRate={0.65}
+      />
+    </Canvas>
+  );
+}
