82fbe841cbfbb28a20626c847830a8bd69a52a69
Alfie Sargent <Alfonse179@gmail.com>
2024-10-04 13:16:44 +0100

    Added in some reflections

--- src/models/DeskModel.tsx
+++ src/models/DeskModel.tsx
@@ -25,28 +25,9 @@ export function Desk(props: JSX.IntrinsicElements['group']) {
         receiveShadow
         geometry={nodes.Desk.geometry}
         material={materials.Desk_Texture}
-        position={[2.0222, 0.6, -1.4116]}
-        scale={[1.2, 1, 0.9103]}
+        position={[0, 0.7, 0]}
+        scale={[1, 1, 1]}
       />
-
-      <mesh ref={overlayRef} position={[1.1, 0.8, -1.07]} rotation={[0, 0, 0]}>
-        <planeGeometry args={[2, 1]} /> {/* Adjust width and height as necessary */}
-        <MeshReflectorMaterial
-          blur={[1000, 1000]}
-          resolution={1024}
-          mirror={0.5}
-          mixBlur={0.4}
-          mixStrength={100}
-          roughness={0.1}
-          depthScale={0}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          color="#050505"
-          metalness={0.05}
-          transparent={true}
-          opacity={0.8}
-        />
-      </mesh>
     </group>
   );
 }
--- src/models/Envrionment.tsx
+++ src/models/Envrionment.tsx
@@ -1,18 +1,41 @@
-
-import React from 'react';
+// src/models/environment.tsx
+import { Reflector, useTexture } from '@react-three/drei';
+import { Vector2 } from 'three';
 
 export function Environment() {
+  // Load textures for the normal and roughness maps
+  const [floor, normal] = useTexture([
+    './SurfaceImperfections003_1K_var1.jpg', // Roughness map
+    './SurfaceImperfections003_1K_Normal.jpg', // Normal map
+  ]);
+
   return (
     <group>
       {/* Lighting setup */}
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={1} castShadow />
-      
-      {/* Ground plane */}
-      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-        <planeGeometry args={[10, 10]} />
-        <meshStandardMaterial color="lightgray" />
-      </mesh>
+      <ambientLight intensity={1.5} />
+      <pointLight position={[10, 10, 10]} intensity={0.1} castShadow />
+
+      {/* Reflective ground plane */}
+      <Reflector
+        blur={[400, 100]} // Blur the reflection
+        resolution={1024} // Reflection resolution
+        args={[50, 50]} // Plane size
+        mirror={0.5} // Reflection intensity
+        mixBlur={6} // Blurriness of the reflection
+        mixStrength={1.5} // Reflection strength
+        rotation={[-Math.PI / 2, 0, 0]} // Rotate to flat plane
+      >
+        {(Material, props) => (
+          <Material
+            color="#a0a0a0" // Base color
+            metalness={0.4} // Metalness
+            roughnessMap={floor} // Apply roughness map
+            normalMap={normal} // Apply normal map
+            normalScale={new Vector2(2, 2)} // Use Vector2 for normalScale
+            {...props}
+          />
+        )}
+      </Reflector>
     </group>
   );
 }
